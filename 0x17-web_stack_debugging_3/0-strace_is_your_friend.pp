1bcf6cacf85c290918eb0322c3c5654bd0175dec
Rayane <rayanetoko@gmail.com>
2024-08-14 19:34:35 -0700

    done with 0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Contain Puppet code that fix apache 500 code error returning.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
