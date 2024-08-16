f5f2ff9b3b534cde178ef2699415e3653141f994
Rayane <rayanetoko@gmail.com>
2024-08-15 18:48:09 -0700

    fix tasks on 0x16. API advance v2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,8 +13,8 @@ def number_of_subscribers(subreddit):
         "User-Agent": "Custom"
     }
     res = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
+    if res.status_code == 200:
+        data = res.json()
         return data['data']['subscribers']
     else:
         return 0
